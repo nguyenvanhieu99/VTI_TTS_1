dc7a613952f7991c3c89e476173295f80c42430e
VanHieu <nvhieu.dev@gmail.com>
2021-07-14 11:58:24 +0700

    ex3

---
+++ src/EX3/Car.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EX3;
+
+/**
+ *
+ * @author van hieu
+ */
+public class Car {
+    private int speed;
+    private double  regularPrice;
+    private String color;
+
+    public Car() {
+    }
+   
+    public double getSalePrice(){
+        return 1;
+    }
+        
+}
